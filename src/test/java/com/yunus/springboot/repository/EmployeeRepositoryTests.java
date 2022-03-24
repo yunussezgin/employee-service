b3c47cf669e79cfe1658e1473af2d19f5d65e6c5
Yunus Sezgin <yunussezgin8@hotmail.com>
2022-03-24 15:22:50 +0300

    Test repository for get employeesList

--- src/test/java/com/yunus/springboot/repository/EmployeeRepositoryTests.java
+++ src/test/java/com/yunus/springboot/repository/EmployeeRepositoryTests.java
@@ -1,6 +1,9 @@
 package com.yunus.springboot.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +18,7 @@ public class EmployeeRepositoryTests {
 	private EmployeeRepository employeeRepository;
 
 	// JUnit test for save employee operation
-	//@DisplayName("JUnit test for save employee operation")
+	@DisplayName("JUnit test for save employee operation")
 	@Test
 	public void givenEmployeeObject_whenSave_thenReturnSavedEmployee() {
 
@@ -34,4 +37,35 @@ public class EmployeeRepositoryTests {
 		assertThat(savedEmployee.getId()).isGreaterThan(0);
 		
 	}
+	
+	// JUnit test for get all employees operation
+	@DisplayName("JUnit test for get all employees operation")
+	@Test
+	public void givenEmployeesList_whenFindAll_thenEmployeesList() {
+		// given - precondition or setup
+		Employee employee = Employee.builder()
+				.firstName("Yunus")
+				.lastName("Sezgin")
+				.email("yunusssezgin8@gmail.com")
+				.build();
+		
+		Employee employee1 = Employee.builder()
+				.firstName("Yunus")
+				.lastName("Zengin")
+				.email("yunusszengin8@gmail.com")
+				.build();
+		
+		employeeRepository.save(employee);
+		employeeRepository.save(employee1);
+		
+		// when - action or the behaviour that we are going test
+		List<Employee> employeelist = employeeRepository.findAll();
+		
+		// then - verify the output
+		assertThat(employeelist).isNotNull();
+		assertThat(employeelist.size()).isEqualTo(2);
+	
+	}
+	
+	
 }
