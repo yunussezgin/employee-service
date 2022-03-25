de6d95b2c042d013898b7020378aa6750d604d0d
Yunus Sezgin <yunussezgin8@hotmail.com>
2022-03-25 16:24:47 +0300

    Test service using @Mock and @InjectMocks

--- src/test/java/com/yunus/springboot/service/EmployeeServiceTests.java
+++ src/test/java/com/yunus/springboot/service/EmployeeServiceTests.java
@@ -8,22 +8,38 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
-import org.mockito.Mockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.BDDMockito.given;
 
 import com.yunus.springboot.model.Employee;
 import com.yunus.springboot.repository.EmployeeRepository;
 import com.yunus.springboot.service.impl.EmployeeServiceImp;
 
+@ExtendWith(MockitoExtension.class)
 public class EmployeeServiceTests {
 	
+	@Mock
 	private EmployeeRepository employeeRepository;
-	private EmployeeService employeeService;
+	
+	@InjectMocks
+	private EmployeeServiceImp employeeService;
+	
+	Employee employee;
 	
 	@BeforeEach
 	public void setup() {
-		employeeRepository = Mockito.mock(EmployeeRepository.class);
-		employeeService = new EmployeeServiceImp(employeeRepository);
+//		employeeRepository = Mockito.mock(EmployeeRepository.class);
+//		employeeService = new EmployeeServiceImp(employeeRepository);
+		employee = Employee.builder()
+				.id(1L)
+				.firstName("Yunus")
+				.lastName("Sezgin")
+				.email("yunussezgin8@gmail.com")
+				.build();
 	}
 	
 	// JUnit test for saveEmployee method
@@ -31,16 +47,10 @@ public class EmployeeServiceTests {
 	@Test
 	public void givenEmployeeObject_whenSaveEmployee_thenReturnEmployeeObject() {
 		// given - precondition or setup
-		Employee employee = Employee.builder()
-				.id(1L)
-				.firstName("Yunus")
-				.lastName("Sezgin")
-				.email("yunussezgin8@gmail.com")
-				.build();
-		BDDMockito.given(employeeRepository.findByEmail(employee.getEmail()))
+		given(employeeRepository.findByEmail(employee.getEmail()))
 				.willReturn(Optional.empty());
 				
-		BDDMockito.given(employeeRepository.save(employee)).willReturn(employee);
+		given(employeeRepository.save(employee)).willReturn(employee);
 		
 		System.out.println(employeeRepository);
 		System.out.println(employeeService);
